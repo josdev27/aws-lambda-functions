309e8affc6296d405950cb48efd2fd31d6421247
josdev27 <47904226+josdev27@users.noreply.github.com>
2020-03-10 11:26:39 +0100

    Create delete-event-item.py

---
+++ delete-event-item.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import boto3
+import json
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+
+    print('Initiating Function...')
+    print("Received event from API Gateway: " + json.dumps(event, indent=2))
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('table-name')
+    
+    try:
+        response = table.delete_item(Key={"id":event["id"]})
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        print('Check your DynamoDB table...')
+    else:
+        print("GetItem succeeded:")
+        print("Received response from DynamoDB: " + json.dumps(response, indent=2))
+        return dict()
