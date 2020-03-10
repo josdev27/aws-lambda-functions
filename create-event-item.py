d80955b579547e0c09d6cea1592f4eda37a5174a
josdev27 <47904226+josdev27@users.noreply.github.com>
2020-03-10 11:28:27 +0100

    Create create-event-item.py

---
+++ create-event-item.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import boto3
+import json
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
+import uuid
+
+def lambda_handler(event, context):
+
+    print('Initiating Function...')
+    print("Received event from API Gateway: " + json.dumps(event, indent=2))
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('table-name')
+    try:
+        event["id"] = str(uuid.uuid4())
+        response = table.put_item(Item=event)
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        print('Check your DynamoDB table...')
+    else:
+        print("PutItem succeeded:")
+        print("Received response from DynamoDB: " + json.dumps(response, indent=2))
+        return event
